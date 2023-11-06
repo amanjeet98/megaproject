61fc0b9d176f6e432bf5fa7f1fc8028a904e57a8
amanjeet singh <amanjeetsingh@amanjeets-MacBook-Air.local>
2023-11-06 19:38:26 +0545

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Logo from "./Logo";
+import Container from "./container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import Input from "./Input";
+import Signup from "./SignUp";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from './AuthLayout'
+import RTE from "./RTE";
+import Select from "./Select";
+ 
+export {
+ Header,
+ Footer,
+ Logo,
+ Container,
+ LogoutBtn,
+ Button,
+ Input,
+ Signup,
+ Login,
+ PostForm,
+ PostCard,
+ AuthLayout,
+ RTE,
+ Select
+}
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,11 @@
+import { Signup as SignupComponenet } from "../components"
+
+function Signup() {
+  return (
+    <div className="py-8">
+     <SignupComponenet/>
+    </div>
+  )
+}
+
+export default Signup
